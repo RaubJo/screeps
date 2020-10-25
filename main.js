b478eb613ac5beccbcf6317df00de31df2fb4507
RaubJo <JosephRaub98@gmail.com>
2020-10-24 21:06:07 -0500

    Implemented longDistanceHarvester code

--- main.js
+++ main.js
@@ -27,19 +27,21 @@ module.exports.loop = function () {
 	var minWallUpgraders = 2;
 	var minRepairers = 2;
 	var minMiners = 1;
+	var W18S21_harvester = 1;
 	var totalHarvesters = _.sum(Game.creeps, (c) => c.memory.role == 'harvester');
 	var totalBuilders = _.sum(Game.creeps, (c) => c.memory.role == 'builder');
 	var totalUpgraders = _.sum(Game.creeps, (c) => c.memory.role == 'upgrader');
 	var totalWallUpgraders = _.sum(Game.creeps, (c) => c.memory.role == 'wallUpgrader');
 	var totalRepairers = _.sum(Game.creeps, (c) => c.memory.role == 'repairer');
 	var totalMiners = _.sum(Game.creeps, (c) => c.memory.role == 'miner');
+	var W18S21_harvester_total = _.sum(Game.creeps, (c) => c.memory.role == 'longDistanceHarvester' && c.memory.target == "W18S21");
 
 	var energy = Game.spawns.Spawn1.room.energyCapacityAvailable;
 	var name = undefined;
 
 	if (totalHarvesters < minHarvesters) {
 		/*name = Game.spawns.Spawn1.createCreep([WORK,WORK,CARRY,MOVE], undefined,
-			{ role: 'harvester', isWorking: false});*/
+			{ role: 'harvester', isWorking: false});*/
 		name = Game.spawns.Spawn1.createCustomCreep(energy, 'harvester');
 
 		if(name == ERR_NOT_ENOUGH_ENERGY && totalHarvesters == 0){
@@ -60,6 +62,9 @@ module.exports.loop = function () {
 		}
 	else if(totalWallUpgraders < minWallUpgraders){
 	    name = Game.spawns.Spawn1.createCustomCreep(energy, "wallUpgrader");
+	}
+	else if(W18S21_harvester_total < W18S21_harvester){
+			name = Game.spawns.Spawn1.createLongDistanceHarvester(energy,3,"W17S21","W18S21",0);
 	}
 
 	if (!(name < 0 || name == undefined)) {
