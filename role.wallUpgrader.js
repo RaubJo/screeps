9492b59b287f7b6a345001644b0421403bb1ff66
RaubJo <JosephRaub98@gmail.com>
2020-10-27 21:33:53 -0500

    ..

--- role.wallUpgrader.js
+++ role.wallUpgrader.js
@@ -17,10 +17,7 @@ module.exports = {
 			}
 		}
 		else {
-			var source = creep.pos.findClosestByPath(FIND_SOURCES);
-			if(creep.harvest(source) == ERR_NOT_IN_RANGE){
-				creep.moveTo(source);
-			}
+			creep.getEnergy();
 		}
 	}
 };
